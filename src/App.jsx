efca1234e07e30f25da021fd71e8b18c3b6e39d0
Moutch11 <e.murat75@gmail.com>
2024-03-22 21:43:23 +0100

    header css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faList } from "@fortawesome/free-solid-svg-icons";
+import { faList, faRectangleList } from "@fortawesome/free-solid-svg-icons";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Task from "./components/Task/Task";
 import TaskAdd from "./components/TaskAdd/TaskAdd";
 
-library.add(faList);
+library.add(faList, faRectangleList);
 
 import "./App.css";
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-      <FontAwesomeIcon icon="list" />
+      <FontAwesomeIcon className="iconlist" icon="rectangle-list" />
       <h1>Todo List</h1>
     </header>
   );
